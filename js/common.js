00e4b65a7a0beac5abc373aa7ee556ace0bf9555
Tinsson <695724046@qq.com>
2017-06-25 23:11:01 +0800

    2

--- js/common.js
+++ js/common.js
@@ -1,3 +1,43 @@
+
+$.fn.autotype = function(callback){
+
+    var $text = $(this);
+    var str = $text.html();
+    var index = 0;
+    var x = $text.html('');
+    if($("#third_box").css("display") == "none"){
+        $("#third_box").show();
+	}
+    var timer = setInterval(function(){
+        var current = str.substr(index, 1);
+        if(current == '<'){
+            index = str.indexOf('>', index) + 1;
+        }else{
+            index ++ ;
+        }
+        $text.html(str.substring(0, index) + (index & 1 ? '_': ''));
+        if(index >= str.length){
+            clearInterval(timer);
+            $(".normal_tip").addClass("font-fade");
+            callback();
+        }
+    },100);
+};
+
+function showtext(){
+    $(".text").eq(0).addClass("animated flipInX");
+    $(".text").eq(1).addClass("animated flipInX");
+    $(".text").eq(1).one("webkitAnimationEnd animationend",function(){
+        $(".text").eq(0).removeClass("animated flipInX").css("opacity",1);
+        $(".text").eq(1).removeClass("animated flipInX").css("opacity",1);
+        $(".text").eq(2).addClass("animated flipInX");
+        $(".text").eq(3).addClass("animated flipInX");
+        $(".text").eq(2).one("webkitAnimationEnd animationend",function(){
+            $(".text").eq(2).removeClass("animated flipInX").css("opacity",1);
+            $(".text").eq(3).removeClass("animated flipInX").css("opacity",1);
+        });
+	})
+}
 $(document).ready(function(){
 	$("#fullpage").onepage_scroll({
 		sectionContainer: ".section",
@@ -21,6 +61,19 @@ $(document).ready(function(){
 						picShow(1);
 					})
 				})
+			}else if(index == 3){
+				$("#third_box").autotype(showtext);
+			}else if(index == 4){
+                $("#ft1").addClass("animated bounceInUp");
+                $("#ft1").one("webkitAnimationEnd animationend",function(){
+                    $("#ft2").addClass("animated bounceInUp");
+                    $("#ft1").css("opacity",1);
+                    $("#ft2").one("webkitAnimationEnd animationend",function(){
+                        $("#ft1").removeClass("animated bounceInUp").addClass("font-fade");
+                        $("#ft2").removeClass("animated bounceInUp").addClass("font-fade");
+                        picShow(2);
+                    })
+                })
 			}
 		},
 		loop: false,
@@ -47,19 +100,28 @@ $(document).ready(function(){
 					})
 				})
 			});
+		}else if(no == 2){
+			//触发拼图的开始
 		}
 	}
-	$(".pic").filter(function(index){
-		if(index == 0 || index == 2){
-			return true;
+	$(".pic").on("click",function () {
+		var i = $(this).index();
+		if(i == 0 || i == 2){
+            $("#fullpage").moveDown();
+		}else{
+
 		}
-	}).on("click",function () {
-		$("#fullpage").moveDown();
-	})
-	$("#rcpage1").on("click",function(){
-		console.log(11111);
 	})
 
+    $(".text").on("click",function () {
+        var i = $(this).index();
+        if(i == 2){
+            $("#fullpage").moveDown();
+
+        }else{
+
+        }
+    })
     var slider = new SliderUnlock("#slider", {
 		labelTip: "滑动解锁",
 		successLabelTip: "解锁成功"
@@ -67,7 +129,7 @@ $(document).ready(function(){
     	$('.mask').addClass('fade-sm');
     	setTimeout(function(){
             $('.mask').hide();
-			ado.play();
+			/*ado.play();*/
 			setTimeout(function(){
 				$(".arrow").addClass("animated bounceInUp");
 				$(".arrow").one("webkitAnimationEnd animationend",function(){
